e96c2092212336f98e0823f18baf2be6b6e44a62
Nikunj Tyagi <86315974+nikunjt0@users.noreply.github.com>
2023-04-07 22:29:00 -0500

    Update trainingModel.py

--- trainingModel.py
+++ trainingModel.py
@@ -9,17 +9,19 @@ import cv2
 import sys
 import torch.nn as nn
 import torch.optim as optim
+import torchvision.transforms as transforms
+from PIL import Image
 
 tumor = []
 healthy = []
-for f in glob.iglob("./data/brain_tumor_dataset/yes/*.jpg"):
+for f in glob.iglob("./data/brain_tumor_dataset/yes/*.png"):
     img = cv2.imread(f)
     img = cv2.resize(img,(128,128))
     b, g, r = cv2.split(img)
     img = cv2.merge([r,g,b])
     tumor.append(img)
 
-for f in glob.iglob("./data/brain_tumor_dataset/no/*.jpg"):
+for f in glob.iglob("./data/brain_tumor_dataset/no/*.png"):
     img = cv2.imread(f)
     img = cv2.resize(img,(128,128)) 
     b, g, r = cv2.split(img)
@@ -76,7 +78,6 @@ class MRIDataset(Dataset):
         img = torch.tensor(img, dtype=torch.float32).permute(2, 0, 1) / 255.0
 
         return img, label
-
 # Split data into train and validation sets
 train_size = 0.8
 train_healthy = healthy[:int(len(healthy) * train_size)]
@@ -84,7 +85,7 @@ train_tumor = tumor[:int(len(tumor) * train_size)]
 val_healthy = healthy[int(len(healthy) * train_size):]
 val_tumor = tumor[int(len(tumor) * train_size):]
 
-# Create train and validation datasets
+# Create train and validation datasets using the new MRIDataset class
 train_dataset = MRIDataset(train_healthy, train_tumor)
 val_dataset = MRIDataset(val_healthy, val_tumor)
 
@@ -115,125 +116,41 @@ optimizer = optim.Adam(model.parameters(), lr=0.001)
 # Define loss function
 criterion = nn.CrossEntropyLoss()
 
+# Define data loaders using the new MRIDataset class
+train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=64, shuffle=False)
 
-#second half of training model
-# define the labels for healthy and tumor images
-healthy_labels = np.zeros(healthy.shape[0], dtype=np.int64)
-tumor_labels = np.ones(tumor.shape[0], dtype=np.int64)
-
-# concatenate the labels for all images
-All_labels = np.concatenate((healthy_labels, tumor_labels))
-
-# define a custom PyTorch dataset for loading the data
-class MRIDataset(Dataset):
-    def __init__(self, data, labels, transform=None):
-        self.data = data
-        self.labels = labels
-        self.transform = transform
-    
-    def __len__(self):
-        return len(self.data)
-    
-    def __getitem__(self, idx):
-        x = self.data[idx]
-        y = self.labels[idx]
-        
-        if self.transform:
-            x = self.transform(x)
-        
-        return x, y
-
-# define a transform to apply to the images
-class MRITransform:
-    def __init__(self, mean, std):
-        self.mean = mean
-        self.std = std
-    
-    def __call__(self, x):
-        x = x / 255.0
-        x = (x - self.mean) / self.std
-        x = np.transpose(x, (2, 0, 1))
-        x = torch.from_numpy(x).float()
-        return x
-
-# define the mean and standard deviation of the image dataset
-mean = np.mean(All / 255.0)
-std = np.std(All / 255.0)
-
-# define the batch size for the data loader
-batch_size = 32
-
-# create a custom PyTorch dataset for loading the data
-dataset = MRIDataset(All, All_labels, transform=MRITransform(mean, std))
-
-# split the dataset into training and validation sets
-train_size = int(0.8 * len(dataset))
-val_size = len(dataset) - train_size
-train_dataset, val_dataset = torch.utils.data.random_split(dataset, [train_size, val_size])
-
-# create data loaders for the training and validation sets
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
-
-# define the model architecture
-model = torch.nn.Sequential(
-    torch.nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
-    torch.nn.ReLU(),
-    torch.nn.MaxPool2d(kernel_size=2, stride=2),
-    torch.nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
-    torch.nn.ReLU(),
-    torch.nn.MaxPool2d(kernel_size=2, stride=2),
-    torch.nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
-    torch.nn.ReLU(),
-    torch.nn.MaxPool2d(kernel_size=2, stride=2),
-    torch.nn.Flatten(),
-    torch.nn.Linear(128 * 16 * 16, 64),
-    torch.nn.ReLU(),
-    torch.nn.Linear(64, 2),
-)
-
-# define the loss function and optimizer
-criterion = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-
-# define the device to use for training
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-# move the model to the device
-model.to(device)
-
-correct = 0
-total = 0
-
-with torch.no_grad():
+# Train the model
+num_epochs = 30
+for epoch in range(num_epochs):
+    # Train the model on the training set
+    model.train()
+    train_loss = 0
+    train_acc = 0
     for images, labels in train_loader:
-        images, labels = images.to(device), labels.to(device)
+        optimizer.zero_grad()
         outputs = model(images)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        train_loss += loss.item() * images.size(0)
+        _, preds = torch.max(outputs, 1)
+        train_acc += torch.sum(preds == labels.data)
+    train_loss /= len(train_loader.dataset)
+    train_acc = train_acc.double() / len(train_loader.dataset)
 
-        # get the first image in the batch
-        img = images[0]
-        # denormalize image tensor
-        img = img * 0.5 + 0.5
-        # convert image tensor to numpy array
-        img = img.cpu().numpy()
-        # transpose image array to match the shape required by imshow
-        img = np.transpose(img, (1, 2, 0))
-        
-        # get the predicted label for the first image in the batch
-        pred_label = predicted[0].item()
-        # get the correct label for the first image in the batch
-        true_label = labels[0].item()
+    # Evaluate the model on the validation set
+    model.eval()
+    val_loss = 0
+    val_acc = 0
+    with torch.no_grad():
+        for images, labels in val_loader:
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            val_loss += loss.item() * images.size(0)
+            _, preds = torch.max(outputs, 1)
+            val_acc += torch.sum(preds == labels.data)
+    val_loss /= len(val_loader.dataset)
+    val_acc = val_acc.double() / len(val_loader.dataset)
 
-        # show the image, predicted label, and correct label
-        fig, ax = plt.subplots()
-        ax.imshow(img)
-        ax.set_title(f"Predicted: {pred_label}, True: {true_label}")
-        plt.show()
-
-
-accuracy = 100 * correct / total
-print('Accuracy of the network on the test images: %d %%' % accuracy)
-
+    print(f"Epoch {epoch+1}/{num_epochs}: Train Loss: {train_loss:.4f}, Train Acc: {train_acc:.4f}, Val Loss: {val_loss:.4f}, Val Acc: {val_acc:.4f}")
